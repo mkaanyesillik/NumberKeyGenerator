655e572910c81e1ed5a64a9011ceae70afd61140
Kaan Yeşillik <mkaanyesillik@gmail.com>
2021-09-04 23:12:54 +0300

    v0.1
    almost done

---
+++ KeyListOluşturucu/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace KeyListOluşturucu
+{
+    partial class FormKeyListOlustur
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormKeyListOlustur));
+            this.btnOlustur = new System.Windows.Forms.Button();
+            this.BgWorker = new System.ComponentModel.BackgroundWorker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtSayiBir = new System.Windows.Forms.TextBox();
+            this.txtSayiIki = new System.Windows.Forms.TextBox();
+            this.lstKeyList = new System.Windows.Forms.ListBox();
+            this.btnKaydet = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // btnOlustur
+            // 
+            this.btnOlustur.Location = new System.Drawing.Point(9, 529);
+            this.btnOlustur.Name = "btnOlustur";
+            this.btnOlustur.Size = new System.Drawing.Size(110, 33);
+            this.btnOlustur.TabIndex = 0;
+            this.btnOlustur.Text = "Oluştur";
+            this.btnOlustur.UseVisualStyleBackColor = true;
+            this.btnOlustur.Click += new System.EventHandler(this.btnOlustur_Click);
+            // 
+            // BgWorker
+            // 
+            this.BgWorker.WorkerReportsProgress = true;
+            this.BgWorker.DoWork += new System.ComponentModel.DoWorkEventHandler(this.BgWorker_DoWork);
+            this.BgWorker.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.BgWorker_ProgressChanged);
+            this.BgWorker.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.BgWorker_RunWorkerCompleted);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "İlk Sayı:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 39);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "İkinci Sayı:";
+            // 
+            // txtSayiBir
+            // 
+            this.txtSayiBir.Location = new System.Drawing.Point(71, 10);
+            this.txtSayiBir.Name = "txtSayiBir";
+            this.txtSayiBir.Size = new System.Drawing.Size(161, 20);
+            this.txtSayiBir.TabIndex = 3;
+            this.txtSayiBir.Text = "0";
+            this.txtSayiBir.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtSayiBir_KeyPress);
+            // 
+            // txtSayiIki
+            // 
+            this.txtSayiIki.Location = new System.Drawing.Point(71, 36);
+            this.txtSayiIki.Name = "txtSayiIki";
+            this.txtSayiIki.Size = new System.Drawing.Size(161, 20);
+            this.txtSayiIki.TabIndex = 4;
+            this.txtSayiIki.Text = "0";
+            this.txtSayiIki.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtSayiIki_KeyPress);
+            // 
+            // lstKeyList
+            // 
+            this.lstKeyList.FormattingEnabled = true;
+            this.lstKeyList.Location = new System.Drawing.Point(9, 62);
+            this.lstKeyList.Name = "lstKeyList";
+            this.lstKeyList.Size = new System.Drawing.Size(226, 446);
+            this.lstKeyList.TabIndex = 5;
+            // 
+            // btnKaydet
+            // 
+            this.btnKaydet.Location = new System.Drawing.Point(125, 529);
+            this.btnKaydet.Name = "btnKaydet";
+            this.btnKaydet.Size = new System.Drawing.Size(110, 33);
+            this.btnKaydet.TabIndex = 6;
+            this.btnKaydet.Text = "Kaydet";
+            this.btnKaydet.UseVisualStyleBackColor = true;
+            this.btnKaydet.Click += new System.EventHandler(this.btnKaydet_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(9, 513);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(226, 10);
+            this.progressBar1.Step = 1000000000;
+            this.progressBar1.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.progressBar1.TabIndex = 7;
+            // 
+            // FormKeyListOlustur
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(244, 571);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.btnKaydet);
+            this.Controls.Add(this.lstKeyList);
+            this.Controls.Add(this.txtSayiIki);
+            this.Controls.Add(this.txtSayiBir);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnOlustur);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(260, 610);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(260, 610);
+            this.Name = "FormKeyListOlustur";
+            this.Text = "KeyList Oluşturucu | Kaan Yeşillik";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOlustur;
+        private System.ComponentModel.BackgroundWorker BgWorker;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtSayiBir;
+        private System.Windows.Forms.TextBox txtSayiIki;
+        private System.Windows.Forms.ListBox lstKeyList;
+        private System.Windows.Forms.Button btnKaydet;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ KeyListOluşturucu/Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KeyListOluşturucu
+{
+    public partial class FormKeyListOlustur : Form
+    {
+        public FormKeyListOlustur()
+        {
+            InitializeComponent();
+            Control.CheckForIllegalCrossThreadCalls = false;
+        }
+
+
+        private void KeyPressListOlusturMetodu()
+        {
+            try
+            {
+                int IlkSayi = Convert.ToInt32(txtSayiBir.Text);
+                int IkinciSayi = Convert.ToInt32(txtSayiIki.Text);
+                progressBar1.Maximum = IkinciSayi;
+
+                if (IlkSayi > IkinciSayi)
+                {
+                    MessageBox.Show("Lütfen İlk Sayıyı Küçük Giriniz.", "KeyList | Kaan Yeşillik");
+                }
+                if (txtSayiBir.TextLength < txtSayiIki.TextLength)
+                {
+                    for (int key = IlkSayi; key <= IkinciSayi; key++)
+                    {
+                        string BasaSifirEkle = key.ToString().PadLeft(txtSayiIki.TextLength, '0');
+                        lstKeyList.Items.Add(BasaSifirEkle);
+                        BgWorker.ReportProgress(key);
+
+                    }
+                }
+            }
+            catch (Exception)
+            {
+
+
+                if (txtSayiBir.Text == "")
+                {
+                    MessageBox.Show("Lütfen İlk Sayıyı Giriniz.", "KeyList | Kaan Yeşillik");
+                }
+                if (txtSayiIki.Text == "")
+                {
+                    MessageBox.Show("Lütfen İkinci Sayıyı Giriniz.", "KeyList | Kaan Yeşillik");
+                }
+
+            }
+
+
+        }
+
+        private void txtSayiBir_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (char.IsLetter(e.KeyChar) || char.IsSymbol(e.KeyChar) || char.IsWhiteSpace(e.KeyChar) || char.IsPunctuation(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void txtSayiIki_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (char.IsLetter(e.KeyChar) || char.IsSymbol(e.KeyChar) || char.IsWhiteSpace(e.KeyChar) || char.IsPunctuation(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+        public void btnOlustur_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                lstKeyList.Items.Clear();
+                BgWorker.RunWorkerAsync();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Birşeyler Ters Gitti. Lütfen Programı Kapatıp Tekrar Açınız.", "KeyList | Kaan Yeşillik");
+                
+            }
+           
+
+        }
+        private void BgWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            KeyPressListOlusturMetodu();
+
+        }
+
+        private void btnKaydet_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.Filter = "KeyList|*.txt";
+            saveFileDialog1.FilterIndex = 1;
+            DialogResult dk = saveFileDialog1.ShowDialog();
+            if (dk == DialogResult.OK)
+            {
+
+
+                try
+                {
+
+                    StreamWriter s = new StreamWriter(saveFileDialog1.FileName);
+                    for (int i = 0; i < lstKeyList.Items.Count; i++)
+                    {
+
+                        s.WriteLine(lstKeyList.Items[i].ToString());
+
+                    }
+                    s.Close();
+                    MessageBox.Show("Key Listesi " + saveFileDialog1.FileName + " Konumuna Kaydedildi.","KeyList | Kaan Yeşillik");
+                }
+                catch
+                {
+
+                    MessageBox.Show("Bir Sorun Oluştu Lütfen Tekrar Deneyin.", "KeyList | Kaan Yeşillik");
+
+                }
+            }
+            else
+            {
+                MessageBox.Show("Lütfen Konum Seçerek ve İsim Yazarak Tekrar Deneyiniz", "KeyList | Kaan Yeşillik");
+            }
+        }
+        private void BgWorker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            progressBar1.Value = e.ProgressPercentage;
+        }
+        private void BgWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            MessageBox.Show("Key Listeniz Hazırdır. Kaydetme İşlemi Yapabilirsiniz.", "KeyList | Kaan Yeşillik");
+        }
+
+
+    }
+}
+
